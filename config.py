6e9ea066ea6026cc4de9f11f34bacd2ae9e5f735
Sahan Sandaruwan <84913793+sahansandaruwan@users.noreply.github.com>
2023-02-15 20:56:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,11 +2,11 @@
 config = {
   'pic_url' : 'https://sandaruwan-img-host.pages.dev/88976667.png',
 
-  'TOKEN' : 'MTA2MjA4NTk2Njc3ODYxNzkxNg.GhWrht.yW6Yl_tkh-B3qTYzdaouV0KKzwsL2oeyD7AzGc',
+  'TOKEN' : 'Token',
 
   'bot_name': '🈹 Big Boss',
 
-  'channel_id':'1062079404664164372',
+  'channel_id':'Channel_ID',
 
   'alive_msg':'Hi I Am Alive',
 
